fe7ec71f46c4ead1b1c15e7e4ec0579ed6f240a9
unknown <pokarom203@gmail.com>
2026-01-12 17:40:13 +0530

    first commit

---
+++ 1_basics/1_variable.js
@@ -0,0 +1,20 @@
+const accountId = 1234
+let accountEmail = "om@gmail.com"
+var accountPassword = "1234"
+accountCity = "Ahmedabad"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "4321"
+accountCity = "Surat"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState]);
